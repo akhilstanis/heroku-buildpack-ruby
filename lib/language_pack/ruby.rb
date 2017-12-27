ab685357dbb5b9e640f67aec9b0ef43ea5099fba
Akhil Stanislavose <akhil.stanislavose@gmail.com>
2017-12-27 12:28:07 -0800

    Update ruby.rb

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -639,12 +639,11 @@ WARNING
             "RUBYOPT"                       => syck_hack,
             "NOKOGIRI_USE_SYSTEM_LIBRARIES" => "true",
             "JAVA_HOME"                     => noshellescape("#{pwd}/$JAVA_HOME"),
-            "BUNDLE_DISABLE_VERSION_CHECK"  => "true"
+            "BUNDLE_DISABLE_VERSION_CHECK"  => "true",
+            "BUNDLE_BUILD__RUBY-OPENCV"     => "--with-opencv-dir=/app/vendor/opencv"
           }
           env_vars["BUNDLER_LIB_PATH"] = "#{bundler_path}" if ruby_version.ruby_version == "1.8.7"
           puts "Running: #{bundle_command}"
-          pipe("bundle config build.ruby-opencv --with-opencv-dir=/app/vendor/opencv")
-          pipe("bundle config")
           instrument "ruby.bundle_install" do
             bundle_time = Benchmark.realtime do
               bundler_output << pipe("#{bundle_command} --no-clean", out: "2>&1", env: env_vars, user_env: true)
